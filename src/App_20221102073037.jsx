726e24388c31ea41f3d797c261968436c9c45cc8
changhan <momo0170@naver.com>
2022-11-02 07:57:13 +0900

    UI, delete list, add list

---
+++ src/App_20221102073037.jsx
@@ -0,0 +1,66 @@
+import styles from './App.module.css';
+import { BsMoonFill, BsSun, BsTrash, BsTrashFill } from 'react-icons/bs';
+import { useEffect, useRef, useState } from 'react';
+import Todo from './components/Todo';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+  const listRef = useRef(null);
+  const onSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+    if (e.target[0].value !== '') {
+      setTodos((prev) => [
+        ...prev,
+        { id: prev.length + 1, name: e.target[0].value },
+      ]);
+    }
+    e.target[0].value = '';
+  };
+
+  const onDelete = (targetId) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== targetId));
+  };
+
+  console.log(todos);
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <button className={styles.modeBtn}>
+          <BsMoonFill size="18" color="#595959" />
+        </button>
+        <ul className={styles.filter}>
+          <li>All</li>
+          <li>Active</li>
+          <li>Completed</li>
+        </ul>
+      </header>
+      <main className={styles.main}>
+        {todos.map((todo) => (
+          <li
+            className={styles.list}
+            key={todo.id}
+            ref={todo.id === todos.length ? listRef : null}
+          >
+            <Todo
+              todo={todo}
+              onDelete={onDelete}
+              BsTrash={BsTrash}
+              BsTrashFill={BsTrashFill}
+            />
+          </li>
+        ))}
+      </main>
+      <footer className={styles.footer}>
+        <form className={styles.form} onSubmit={onSubmit}>
+          <input type="text" placeholder="Add Todo" className={styles.input} />
+          <button className={styles.addBtn}>Add</button>
+        </form>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../css/Todo.module.css';
+import { BsTrash, BsTrashFill } from 'react-icons/bs';
+
+export default function Todo({ todo, onDelete }) {
+  const handleDelete = () => {
+    onDelete(todo.id);
+  };
+  return (
+    <>
+      <input type="checkbox" className={styles.input} />
+      <span className={styles.name}>{todo.name}</span>
+      <button className={styles.deleteBtn} onClick={handleDelete}>
+        <BsTrashFill size="18" color="#595959" />
+      </button>
+    </>
+  );
+}
